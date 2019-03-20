8bf34a56fc13815f7eb3e4448e66d3370646f42a
Ethan <ethansagin@gmail.com>
2019-03-20 15:54:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -36,8 +36,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-  programmer_hash[:alan_kay][:known_for] = "GUI"
-  programmer_hash
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -103,7 +102,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
      
      
 end
